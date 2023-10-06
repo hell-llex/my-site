bb25b166e5dab24325493d43aa264abe68303c83
hell-llex <demeshenko333@gmail.com>
2023-10-07 01:56:29 +0300

    feat: add page home

--- src/pages/pageHome/index.ts
+++ src/pages/pageHome/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./pageHome";
+export { default } from "./PageHome";
--- src/pages/pageHome/pageHome.tsx
+++ src/pages/pageHome/pageHome.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import BackgroundHomePage from "../../components/BackgroundHomePage";
+import { ImageInfo, TotalInfo } from "../../types";
+// import SocialLinks from "../../components/SocialLinks";
+import { useAppSelector } from "../../hooks/redux";
+import "./PageHome.scss";
+// import { Icon } from "@iconify/react/dist/iconify.js";
 
-const pageHome = () => {
+const PageHome = () => {
+  const dataImages = useAppSelector((state) => state.imagesInfo);
+  // const dataIcons = useAppSelector((state) => state.socialIconsInfo);
+  const [imagesBack] = useState<(ImageInfo | TotalInfo)[]>([
+    dataImages.image_4,
+    dataImages.image_7,
+    dataImages.image_2,
+    dataImages.image_3,
+  ]);
+
   return (
     <>
-      <div>pageHome</div>
+      <div className="home-page">
+        <BackgroundHomePage imgs={imagesBack} width="60vw" />
+      </div>
     </>
   );
 };
 
-export default pageHome;
+export default PageHome;
