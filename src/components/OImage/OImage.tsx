06757d3494f13a76ea3b423be76fa08fff6d92eb
hell-llex <demeshenko333@gmail.com>
2023-10-07 01:23:32 +0300

    feat: add image component

---
+++ src/components/OImage/OImage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+import Image from "react-image-webp";
+import { ImageInfo, TotalInfo } from "../../types";
+import "./OImage.scss";
+
+const OImage = ({ img }: { img: ImageInfo | TotalInfo }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [OImg, setOImg] = useState(img as ImageInfo);
+
+  return (
+    <>
+      {
+        <Image
+          className="optimized-image"
+          src={OImg.pathJpg}
+          webp={OImg.pathWebp}
+          alt={OImg.description}
+        />
+      }
+    </>
+  );
+};
+
+export default OImage;
