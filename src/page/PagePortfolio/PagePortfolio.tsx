6a67f4ea2c864ce3b6daf9d186c8855f1307d988
hell-llex <demeshenko333@gmail.com>
2023-10-11 20:41:27 +0300

    fix: fix style PagePortfolio

--- src/page/PagePortfolio/PagePortfolio.tsx
+++ src/page/PagePortfolio/PagePortfolio.tsx
@@ -1,7 +1,23 @@
-// import React from 'react';
+import { useState } from "react";
+import BackgroundPortfolioPage from "../../components/BackgroundPortfolioPage";
+import { useAppSelector } from "../../hooks/redux";
+import { ImageInfo, TotalInfo } from "../../types";
+import "./PagePortfolio.scss";
 
 const PagePortfolio = () => {
-  return <div>pagePortfolio</div>;
+  const dataImages = useAppSelector((state) => state.imagesInfo);
+  const [imagesBack] = useState<(ImageInfo | TotalInfo)[]>([
+    dataImages.image_4,
+    dataImages.image_7,
+  ]);
+
+  return (
+    <>
+      <div className="portfolio-page">
+        <BackgroundPortfolioPage imgs={imagesBack} width="60vw" />
+      </div>
+    </>
+  );
 };
 
 export default PagePortfolio;
