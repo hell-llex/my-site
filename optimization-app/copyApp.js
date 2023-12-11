26b2adec480b5a26a9ed36667161029e4d796f51
hell-llex <demeshenko333@gmail.com>
2023-12-11 04:26:26 +0300

    feat: add code remove script copy-app for optimization-app-locale

--- optimization-app/copyApp.js
+++ optimization-app/copyApp.js
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import { EOL } from "os";
+import path from 'path';
 
 const sourcePath = '../optimization-app';
 const targetPath = '../optimization-app-local';
@@ -14,8 +15,31 @@ if (fs.existsSync(targetPath)) {
 fs.copy(sourcePath, targetPath, { recursive: true, overwrite: true, dereference: true }, (err) => {
   if (err) {
     console.error('Error copying the application:', err, EOL);
-  } else {
-    console.log('The application has been copied successfully.', EOL);
+    console.log("=".repeat(50));
+    return;
   }
-  console.log("=".repeat(50));
+
+  const packageJsonPath = path.join(targetPath, 'package.json');
+
+  fs.readFile(packageJsonPath, 'utf-8', async (readErr, data) => {
+    if (readErr) {
+      console.error('Error reading package.json:', readErr, EOL);
+      console.log("=".repeat(50));
+      return;
+    }
+    const updatedData = await modifyPackageJson(data);
+    fs.writeFile(packageJsonPath, updatedData, 'utf-8', (writeErr) => {
+      if (writeErr) {
+        console.error('Error writing package.json:', writeErr, EOL);
+      } else {
+        console.log('The application has been copied successfully.', EOL);
+      }
+      console.log("=".repeat(50));
+    });
+  });
 });
+
+async function modifyPackageJson(data) {
+  const newData = data.replace('"copy-app": "node copyApp.js",', "");
+  return newData;
+}
