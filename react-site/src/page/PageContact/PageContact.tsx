edc19ca0e49eacea9291102b2694024d3838a495
hell-llex <demeshenko333@gmail.com>
2024-02-06 02:51:34 +0300

    feat: add 3d element with spline

--- react-site/src/page/PageContact/PageContact.tsx
+++ react-site/src/page/PageContact/PageContact.tsx
@@ -1,68 +1,16 @@
-// import { useState } from "react";
-// import SocialLinkItem from "../../components/SocialLinkItem";
-// import { useAppSelector } from "../../hooks/redux";
-// import { SocialLinksInfo } from "../../types";
-import { Box, Typography } from "@mui/material";
 import "./PageContact.scss";
-import { useTranslation } from "react-i18next";
-// import { v4 as uuidv4 } from "uuid";
-
+import Spline from "@splinetool/react-spline";
 const PageContact = () => {
-  const { t } = useTranslation();
-  // const dataIcons = useAppSelector((state) => state.socialIconsInfo);
-  // const [socialLinksInfo] = useState<SocialLinksInfo>(dataIcons);
   return (
     <div className="contact-page">
-      <Box>
-        <Typography
-          variant="h2"
-          fontWeight={300}
-          fontSize={"clamp(2rem, 3vw, 3.5rem)"}
-        >
-          {t("page.PageContact.title1")}: <br />
-          {t("page.PageContact.textName")}
-        </Typography>
-        <br />
-        <Typography
-          variant="h2"
-          fontWeight={300}
-          fontSize={"clamp(2rem, 3vw, 3.5rem)"}
-        >
-          {t("page.PageContact.title2")}: <br />
-          {t("page.PageContact.textLocation")}
-        </Typography>
-        <br />
-        <Typography
-          variant="h2"
-          fontWeight={300}
-          fontSize={"clamp(2rem, 3vw, 3.5rem)"}
-        >
-          {t("page.PageContact.title3")}: <br />
-          +375(29)289-77-22{" "}
-        </Typography>
-        <br />
-        <Typography
-          variant="h2"
-          fontWeight={300}
-          fontSize={"clamp(2rem, 3vw, 3.5rem)"}
-        >
-          {t("page.PageContact.title4")}: <br />
-          demeshenko333@gmail.com
-        </Typography>
-      </Box>
-      {/* <div className="contact-page__links">
-        {Object.values(socialLinksInfo).map((linkItem) => (
-          <SocialLinkItem
-            link={linkItem}
-            key={uuidv4()}
-            defaultSetting={{
-              defaultOpen: true,
-              placement: "bottom",
-              arrow: true,
-            }}
-          />
-        ))}
-      </div> */}
+      <div
+        style={{
+          width: "100%",
+          height: "80%",
+        }}
+      >
+        <Spline scene="https://prod.spline.design/CCb4ONohlgKdbE4j/scene.splinecode" />
+      </div>
     </div>
   );
 };
